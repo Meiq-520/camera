e0e1c5106e8f1b8a95ce5eaae34f39c696f94955
meiqi <meiqi@qq.com>
2022-07-22 19:20:18 +0800

    11114

---
+++ app/src/main/java/com/er/arctic/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.er.arctic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/er/arctic/MainActivity2.kt
@@ -0,0 +1,11 @@
+package com.er.arctic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
